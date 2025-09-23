db2fbcb0ca8b510e46dde914980e2d29e2cd0b6e
neoadib <neoadib@gmail.com>
2025-09-23 11:27:38 +0200

    update

--- code/task1.py
+++ code/task1.py
@@ -1,6 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
-
+from SPHSolver import SPHSolver
 # import SPHSolver
 
 ## initial conditions
@@ -26,13 +26,6 @@ p_lo = 0.1795 # Pa
 rho_hi = 1 # kg/m
 rho_lo = 0.25 # kg/m
 
-v_hi = 0.0
-v_lo = 0.0
-
-e_hi = 2.5
-e_lo = 1.795
-
-
 N = N_hi+N_lo
 
 # we set evenly spaced particle coordinates
@@ -48,21 +41,18 @@ x_0 = np.concatenate([x_hi, x_lo])
 # initial pressure and density
 p_0 = np.concatenate([np.full(N_hi, p_hi), np.full(N_lo, p_lo)])
 rho_0 = np.concatenate([np.full(N_hi, rho_hi), np.full(N_lo, rho_lo)])
-v_0 = np.concatenate([np.full(N_hi, v_hi), np.full(N_lo, v_lo)])
-e_0 = np.concatenate([np.full(N_hi, e_hi), np.full(N_lo, e_lo)])
+
 ## the state vector shall contain:
 ## x_i = [x_0, 0, 0] , v_i = 0, m_i, rho_i, p_i
 
 ## contsruct the initial state vector X_0
 
-X_0 = np.zeros((N, 11))
+X_0 = np.zeros((N, 9))
 
 X_0[:, 0] = x_0 # set pos
 X_0[:, 6] = np.full(N, m)
 X_0[:, 7] = rho_0
 X_0[:, 8] = p_0
-X_0[:, 9] = v_0
-X_0[:, 10] = e_0
 
 plt.plot(X_0[:, 0], X_0[:, -1])
 
